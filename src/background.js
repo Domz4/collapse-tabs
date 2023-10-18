6df323e3c77e46b8a55a584b14032332ded28df7
Domze <domrza99@gmail.com>
2023-10-18 20:29:11 +0200

    fiding and sorting tabs sripts

--- src/background.js
+++ src/background.js
@@ -1,1 +1,29 @@
-console.log("I'm working just fine here!");
+chrome.tabs.query({}, function (tabs) {
+  const groupedTabs = {};
+
+  tabs.forEach((tab) => {
+    const url = tab.url;
+    const regex = /^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n?]+)/;
+    const matches = url.match(regex);
+    const domain = matches && matches.length > 1 ? matches[1] : null;
+
+    if (domain) {
+      if (groupedTabs.hasOwnProperty(domain)) {
+        groupedTabs[domain].push(tab);
+      } else {
+        groupedTabs[domain] = [tab];
+      }
+    }
+  });
+
+  const sortedGroups = Object.keys(groupedTabs).sort();
+  const sortedTabs = {};
+  sortedGroups.forEach((group) => {
+    sortedTabs[group] = groupedTabs[group];
+  });
+
+  chrome.tabs.create({ url: "popup.html" }); // Open a new tab with the result
+  chrome.runtime.onConnect.addListener(function (port) {
+    port.postMessage(sortedTabs); // Send the sorted tabs to popup.html
+  });
+});
--- src/popup.js
+++ src/popup.js
@@ -1,1 +1,18 @@
-console.log("HERE I AM POPUP DAT DZEJ ES");
+chrome.runtime
+  .connect({ name: "popup" })
+  .onMessage.addListener(function (sortedTabs) {
+    const tabsList = document.getElementById("tabsList");
+    for (const domain in sortedTabs) {
+      const domainTabs = sortedTabs[domain];
+      const domainListItem = document.createElement("li");
+      domainListItem.textContent = domain;
+      const domainTabsList = document.createElement("ul");
+      domainTabs.forEach((tab) => {
+        const tabListItem = document.createElement("li");
+        tabListItem.textContent = tab.title;
+        domainTabsList.appendChild(tabListItem);
+      });
+      domainListItem.appendChild(domainTabsList);
+      tabsList.appendChild(domainListItem);
+    }
+  });
